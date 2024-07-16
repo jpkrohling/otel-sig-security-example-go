3bf01fe2b357ab43db8eae149034bfc30378a201
Juraci Paixão Kröhling <juraci@kroehling.de>
2024-07-16 14:10:20 +0200

    lint
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- internal/telemetry/telemetry.go
+++ internal/telemetry/telemetry.go
@@ -9,20 +9,20 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-func InitTelemetry() func() error {
+func InitTelemetry() (func() error, error) {
 	if otelEnabled() {
 		exp, err := otlptracegrpc.New(context.Background())
 		if err != nil {
-			panic(fmt.Sprintf("failed to initialize traces exporter: %v", err))
+			return nil, fmt.Errorf("failed to initialize traces exporter: %v", err)
 		}
 		tp := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exp))
 		otel.SetTracerProvider(tp)
 		return func() error {
 			return tp.Shutdown(context.Background())
-		}
+		}, nil
 	}
 
 	return func() error {
 		return nil
-	}
+	}, nil
 }
--- main.go
+++ main.go
@@ -11,8 +11,18 @@ import (
 func main() {
 	// added some telemetry just to have a few dependencies on the go.mod, and therefore, a go.sum file
 	ctx := context.Background()
-	close := telemetry.InitTelemetry()
-	defer close()
+	close, err := telemetry.InitTelemetry()
+	if err != nil {
+		fmt.Printf("Failed to initialize telemetry: %v\n", err)
+		return
+	}
+
+	defer func() {
+		err := close()
+		if err != nil {
+			fmt.Printf("Failed to close telemetry: %v\n", err)
+		}
+	}()
 
 	tr := otel.Tracer("github.com/jpkrohling/otel-sig-security-example-go")
 
